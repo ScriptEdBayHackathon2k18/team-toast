7b7e33c270d0961b43e631621f0ecd33164d1469
Glitch (team-toast) <none>
2018-03-25 18:38:35 +0000

    📊🐘 Checkpoint ./script.js:789097/300:789098/282:789100/561 ./index.html:789100/103

--- script.js
+++ script.js
@@ -6,38 +6,41 @@ $('document').ready(function() {
   }
   function beginGame() {
     $('.container').empty();
-    $('.container').append("<p>Sample Room</p>");
+    $('.container').append("<p>Your journey in The Maze begins ...</p>");
     var toasts = 5;
     var unlocked = {
     
     }
+    room1();
   }
                     
   //ME JORDAN        
   function room1() {
     // TODO: make left button, add description of the room, background color, 
-    
+    $(".container").append("<p> Looks lika modern house bedroom witha chest </p>");
   }
   function room2() {
-
+    $(".container").append("<p> Looks lika modern house bedroom witha chest </p>");
   }
   function room3() {
-
+    $(".container").append("<p> Looks lika modern house bedroom witha chest </p>");
   }
   function room4() {
-
+    $(".container").append("<p> Looks lika modern house bedroom witha chest </p>");
   }
   function room5() {
-
+    $(".container").append("<p> Looks lika modern house bedroom witha chest </p>");
   }
                     
                     
-  // WEI                 
+  // WEI
+  // Skeleton !
   function room6() {
-    $()
+    $(".container").append("<p>As you enter to the room, you feel a creepy prescence. A skeleton appears!</p>");
   }
+  // Nothing (?)
   function room7() {
-
+    $(".container").append("<p>You enter an empty room</p>")
   }
   function room8() {
 
@@ -52,7 +55,7 @@ $('document').ready(function() {
 //Alizah
   //have springs, as in water.
   function room11() {
-    
+    //$(".desciption").append(" <p> Now this room has a water fall to go through
 
   }
   //balloon, pop it and have powerup?
