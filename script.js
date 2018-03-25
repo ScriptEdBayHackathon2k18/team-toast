ccf34baf9ef109dd066334349b436e524c72defa
Glitch (team-toast) <none>
2018-03-25 18:01:53 +0000

    🔦☃️ Checkpoint ./index.html:789097/658:789100/1 ./script.js:787558/4:789097/1084:789098/376:789100/61:789117/63

--- script.js
+++ script.js
@@ -1,2 +1,65 @@
-console.log('hi');
+$('document').ready(function() {
+  // JS Starts here
+  function room1() {
 
+  }
+                    
+  //ME              
+  function room2() {
+    
+  }
+  function room3() {
+
+  }
+  function room4() {
+
+  }
+  function room5() {
+
+  }
+  function room6() {
+
+  }
+                    
+                    
+  // WEI                 
+  function room#() {
+
+  }
+  function room#() {
+
+  }
+  function room#() {
+
+  }
+  function room#() {
+
+  }
+  function room#() {
+
+  }
+                    
+// Alizah
+  //  have hf hsprings
+  function room11() {
+
+  }
+  //  
+  function room12() {
+
+  }
+  //
+  function room13() {
+
+  }
+  //
+  function room14() {
+
+  }
+  //
+  function room15() {
+
+  }
+                  
+  
+});
